e912b60a09dd2283dafabb347419a3002494a367
Simon <Simon@gmail.com>
2012-09-05 16:06:31 +0800

    2012-09-05 css body add font-family:open hei;

--- js/searchInput.js
+++ js/searchInput.js
@@ -5,7 +5,7 @@ enyo.kind({
     tag:"div",
     components:[
         {kind: "onyx.InputDecorator", classes:"searchBar",components: [
-            {kind: "onyx.Input",name:"word", published:{value:"", placeholder: ""},ontap:"getKeyborad"}
+            {kind: "onyx.Input",classes:"input_word",name:"word", published:{value:"", placeholder: ""},ontap:"getKeyborad"}
         ]},
         {kind: "onyx.Button", classes:"searchBar",content: "搜尋",ontap:"search"},
     ],
